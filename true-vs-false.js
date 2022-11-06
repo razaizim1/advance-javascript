5ef0e34ccdf078a2edbfa758ae539eb5e497ccba
Razai Zim <zim.sarkar5956@gmail.com>
2022-11-06 06:39:47 +0600

    true-vs-false

---
+++ true-vs-false.js
@@ -0,0 +1,19 @@
+// FALSE
+// 0
+// false
+// ""
+// undefined
+// null
+// NaN
+
+
+// TRUE
+//"0", [], " ",
+
+let age = 5;
+
+if (age || age == 0) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
