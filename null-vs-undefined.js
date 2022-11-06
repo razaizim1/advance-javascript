fe5af3abb50da6f5bbf9f0ae15db7d5d7ae9cb3f
Razai Zim <zim.sarkar5956@gmail.com>
2022-11-06 06:54:24 +0600

    null vs undefined cleared for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,33 @@
+let age;
+console.log(age);
+
+function add (num1, num2) {
+    console.log(num1 + num2);
+}
+
+const result = add(133, 2);
+console.log(result);
+
+function add (num1, num2) {
+    console.log(num1 + num2);
+    return
+}
+
+const result = add(133, 2);
+console.log(result);
+
+function add (num1, num2) {
+    console.log(num1, num2);
+}
+
+const result = add(133);
+console.log(result);
+
+const details = {name: "Razai Zim", roll: "01"}
+console.log(details.section);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [20, 30, 40];
+console.log(ages[20]);
